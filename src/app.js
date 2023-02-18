54ba5362db9c1ebe9563a806acbb1df353032d9e
BenBungle <benstarkart@gmail.com>
2023-02-18 12:42:36 +0000

    domaingenerator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "Grand", "El"];
+  let adj = ["big", "theft", "tio"];
+  let noun = ["kahuna", "auto", "aceite"];
+  let extensions = [".us", ".etc", ".cl"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++)
+      for (let b = 0; b < noun.length; b++)
+        for (let c = 0; c < extensions.length; c++) {
+          console.log(`${pronoun[i]}${adj[j]}${noun[b]}${extensions[c]}`);
+          let div = document.createElement("div");
+          div.innerText = `
+            ${pronoun[i]}${adj[j]}${noun[b]}${extensions[c]}
+            `;
+          document.body.appendChild(div);
+        }
+  }
 };
